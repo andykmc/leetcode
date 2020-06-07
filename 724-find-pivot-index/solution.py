451b9d404420e6712e47a1aefddcabef64d455a1
Andy Kan <andy.kmc@hotmail.com>
2020-06-07 23:47:01 +0100

    Add 724 Find Pivot Index

---
+++ 724-find-pivot-index/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left = 0;
+        right = sum(nums)
+        for i, num in enumerate(nums):
+            if left == right - left - num:
+                return i
+            left += num
+        return -1
